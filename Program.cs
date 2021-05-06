37e6b147fb63af8d8e01ab74f773e779c430c472
jose <jose1914luis@gmail.com>
2021-05-05 21:18:31 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TuyaTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
